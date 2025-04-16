e63f0f6eeb0b1b682974f3e44c83800cd694285d
Emi <emilie.rousseau4@gmail.com>
2025-04-16 12:33:06 +0200

    styles: add scss and modify angular json

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterOutlet, MatIcon],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
